c55b0c83c25b38e0378355ba0aa8cc8f8a802b24
JW HWANG <kgwjddn544@gmail.com>
2024-04-11 17:23:42 +0900

    20240411 1일차

---
+++ day01/cs01_basic_app/ex04_methods/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ex04_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3; int y = 4;
+            BasicSwap(x, y);
+            Console.WriteLine($"BasicSwap -> x = {x}, y = {y}");
+
+            RefSwap(ref x, ref y);  // 참조로 매개 변수 사용할때는 ref를 사용해야함
+            Console.WriteLine($"RefSwap -> x = {x}, y = {y}");
+
+            int a = 10; int b = 3;
+            int 값 = 0; int 나머지 = 0;
+            Divide(a, b, out 값, out 나머지);
+            Console.WriteLine($"{a}/{b} = {값}, {나머지}");
+            Console.WriteLine(Sum(1, 2, 3, 4));
+            Console.WriteLine(Sum(1, 2, 3, 4, 546,4,22));
+            PrintProfile(phone: "010-8790-2222", name: "황정우");
+            
+        }
+        public static void BasicSwap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        public static void RefSwap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        // quotient 나누기 값, remainder 나머지
+        public static void Divide(int a, int b, out int quotient, out int remainder) {
+            quotient = a / b;
+            remainder = a % b;
+            // 예전에 튜플 리턴이 없어서 한번에 하나만 값을 리턴할 수 있음
+        }
+
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static float Plus(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static int Sum(params int[] argv)
+        {
+            int sum = 0;
+            foreach(var item in argv)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        public static void PrintProfile(string name, string phone) {
+            Console.WriteLine($"이름 = {name}, 모바일 = {phone}");
+        }
+        
+        
+    }
+}
---
+++ day01/cs01_basic_app/hello_world/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace hello_world // 프로그램 소스의 가장 큰 단위 namespace == project
+{
+    internal class Program // 접근 제한자 class 파일 명
+    {
+        static void Main(string[] args) // 정적함수 void Main
+        { 
+            // System 네임스페이스 > Console 클래스에 있는 정적 메서드 WriteLine()
+            //Console.WriteLine("Hello, C#!");\
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법 : hello_world.exe <이름>");
+                return;
+
+            }else
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+        }
+    }
+}
